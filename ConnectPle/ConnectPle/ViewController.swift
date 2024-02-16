44b216948921bc1e558824c717b70e1867ee3fcc
lishiche <lishiche@usc.edu>
2024-02-15 19:00:25 -0500

    setup

---
+++ ConnectPle/ConnectPle/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  ConnectPle
+//
+//  Created by Nolan Chen on 2/15/24.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
